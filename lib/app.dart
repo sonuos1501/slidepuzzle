f3acab22fab1574bfbb967d48a1e0f912e77ba88
sonuos1501 <son.uospt@gmail.com>
2023-08-13 17:44:26 +0700

    setup router

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
+
+  static final router = GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          return const Text('Hello');
+        },
+      ),
+    ],
+  );
 
   @override
   State<App> createState() => _AppState();
@@ -10,9 +23,11 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routeInformationParser: App.router.routeInformationParser,
+      routerDelegate: App.router.routerDelegate,
+      debugShowCheckedModeBanner: false,
       onGenerateTitle: (context) => 'Not Found',
-      home: const Text('Hello'),
     );
   }
 }
