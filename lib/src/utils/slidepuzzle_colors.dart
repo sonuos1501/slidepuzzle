968309888d301eed29442ed0fd072a06ff1f6ac3
sonuos1501 <son.uospt@gmail.com>
2023-08-14 20:46:58 +0700

    setup color

--- lib/src/utils/slidepuzzle_colors.dart
+++ lib/src/utils/slidepuzzle_colors.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+class SlidePuzzleColors {
+  static const dark = SlidePuzzleColorPack(
+    blueBg: Color(0xFF12181A),
+    greenBg: Color(0xFF151914),
+    yellowBg: Color(0xFF1B1911),
+    redBg: Color(0xFF1A1512),
+  );
+
+  static const light = SlidePuzzleColorPack(
+    blueBg: Color(0xFFF6FBFE),
+    yellowBg: Color(0xFFFFFDF5),
+    greenBg: Color(0xFFF9FDF8),
+    redBg: Color(0xFFFEF9F6),
+  );
+}
+
 class SlidePuzzleColorPack {
   const SlidePuzzleColorPack({
     required this.blueBg,
