44d5350072a3aca470de092b0e26292ae165efa1
sonuos1501 <son.uospt@gmail.com>
2023-08-14 14:34:11 +0700

    create color class

---
+++ lib/src/utils/slidepuzzle_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SlidePuzzleColorPack {
+  const SlidePuzzleColorPack({
+    required this.blueBg,
+    required this.yellowBg,
+    required this.greenBg,
+    required this.redBg,
+  });
+
+  final Color blueBg;
+  final Color yellowBg;
+  final Color greenBg;
+  final Color redBg;
+}
