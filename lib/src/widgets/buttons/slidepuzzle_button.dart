206337e66768ee2f0921ba571eab538a173d7b1b
sonuos1501 <son.uospt@gmail.com>
2023-08-19 10:50:16 +0700

    create buttons

--- lib/src/widgets/buttons/slidepuzzle_button.dart
+++ lib/src/widgets/buttons/slidepuzzle_button.dart
@@ -1,4 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:slidepuzzle/src/widgets/widgets.dart';
+
+enum SlidepuzzleButtonState { hover, idle, pressed }
+
+enum SlidepuzzleButtonStyle { invert, normal }
+
+class SlidepuzzleButton extends HookConsumerWidget {
+  const SlidepuzzleButton({
+    super.key,
+    required this.color,
+    this.size = ButtonSize.large,
+    this.customSize,
+    this.scale = 1,
+    this.fontSize = 14,
+    required this.onPressed,
+    required this.child,
+    this.style = SlidepuzzleButtonStyle.normal,
+  });
+
+  final ButtonColors color;
+  final ButtonSize size;
+  final SlidepuzzleButtonStyle style;
+  final double fontSize;
+  final VoidCallback onPressed;
+  final Widget child;
+  final double scale;
+  final Size? customSize;
+
+  Color _surfaceColor(ButtonColors color, double value) {
+    return Color.lerp(
+      Color.lerp(Colors.white, color.primaryColor, 0.02),
+      color.primaryColor,
+      style == SlidepuzzleButtonStyle.invert ? 1 - value : value,
+    )!;
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final buttonState = useState(SlidepuzzleButtonState.idle);
+    // TODO: Insert audio controller
+    // final audioController;
+
+    return MouseRegion(
+      cursor: MouseCursor.defer,
+      onEnter: (_) => buttonState.value = SlidepuzzleButtonState.hover,
+      onExit: (_) => buttonState.value = SlidepuzzleButtonState.idle,
+      child: GestureDetector(
+        onTapDown: (_) => buttonState.value = SlidepuzzleButtonState.pressed,
+        onTapUp: (_) => buttonState.value = SlidepuzzleButtonState.idle,
+        onTapCancel: () => buttonState.value = SlidepuzzleButtonState.idle,
+        onTap: () {
+          onPressed();
+        },
+        child: TweenAnimationBuilder<double>(
+          tween: Tween<double>(
+            begin: 0,
+            end: buttonState.value == SlidepuzzleButtonState.hover ? 1 : 0,
+          ),
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.decelerate,
+          builder: (context, value, child) {
+            final brightness = Theme.of(context).brightness;
+            final colorButton = Color.lerp(
+              color.primaryColor,
+              Theme.of(context).colorScheme.background,
+              style == SlidepuzzleButtonStyle.invert ? 1 - value : value,
+            )!;
+            final borderColor = Color.lerp(
+              Color.lerp(
+                colorButton,
+                brightness == Brightness.light ? Colors.black : Colors.white,
+                0.3,
+              )!,
+              color.primaryColor,
+              style == SlidepuzzleButtonStyle.invert ? 1 - value : value,
+            )!;
+
+            return CustomPaint(
+              painter: BorderSlidepuzzlePainter(
+                color: colorButton,
+                borderColor: borderColor,
+                thickness: 2 + value * 1,
+                elevation: 5 + value * 5,
+                edge: 5,
+                brightness: brightness,
+              ),
+              child: IconTheme(
+                data: IconThemeData(color: _surfaceColor(color, value)),
+                child: DefaultTextStyle(
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        fontSize: fontSize * scale,
+                        color: _surfaceColor(color, value),
+                      ),
+                  child: child!,
+                ),
+              ),
+            );
+          },
+          child: SizedBox(
+            height: customSize != null ? customSize!.height : 49 * scale,
+            width: customSize != null
+                ? customSize!.width
+                : size == ButtonSize.square
+                    ? 49 * scale
+                    : 190,
+            child: Center(child: child),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class BorderSlidepuzzlePainter extends CustomPainter {
   const BorderSlidepuzzlePainter({
