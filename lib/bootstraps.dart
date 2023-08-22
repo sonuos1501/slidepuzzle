44922bd782c876283c0375fefb0454d1628f9a4a
sonuos1501 <son.uospt@gmail.com>
2023-08-22 16:34:38 +0700

    add to bootstraps

--- lib/bootstraps.dart
+++ lib/bootstraps.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_portal/flutter_portal.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:slidepuzzle/src/cores/db/db.dart';
+import 'package:slidepuzzle/src/features/app_settings/app_setting_local.dart';
 
 void bootstraps(Widget app) async {
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
@@ -11,5 +13,15 @@ void bootstraps(Widget app) async {
   // Database initialization
   AdapterInitializer.initialize();
 
-  runApp(Portal(child: app));
+  AppSettingLocal appSettingLocal = AppSettingLocalImpl();
+  await appSettingLocal.init();
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        appSettingLocalProvider.overrideWithValue(appSettingLocal),
+      ],
+      child: Portal(child: app),
+    ),
+  );
 }
