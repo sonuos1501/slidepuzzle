d8dcb5bf0582650117b3674c15d2deaee1abdf62
sonuos1501 <son.uospt@gmail.com>
2023-08-24 20:49:35 +0700

    creat mock for app setting local

---
+++ test/features/app_settings/mock_app_setting_local.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:slidepuzzle/src/features/app_settings/app_setting_local.dart';
+
+class MockAppSettingLocal extends Mock implements AppSettingLocal {}
